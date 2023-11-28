1f9ba75dcbe446878e96686c9f241fa394ab6cd3
roshan-bh91 <roshan@betterhalf.ai>
2023-11-28 12:14:32 +0530

    implemented catch all route with docs folder

---
+++ 02-routing/pages/docs/[...params].js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+
+const DocsPage = () => {
+  const router = useRouter();
+  const { params = [] } = router.query;
+  console.log({ params });
+  if (params.length === 2) {
+    return (
+      <h1>
+        Docs Page loaded in case of Concept {params[1]} for feature {params[0]}
+      </h1>
+    );
+  } else if (params.length === 1) {
+    return <h1>Docs Page loaded in case of Feature {params[0]}</h1>;
+  }
+  return <h1>Docs Page loaded</h1>;
+};
+
+export default DocsPage;
