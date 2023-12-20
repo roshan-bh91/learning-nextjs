c616682bc0447f0fb523f24e6b57574cd35ffb0b
roshan-bh91 <roshan@betterhalf.ai>
2023-12-20 12:22:06 +0530

    change in code for index route

--- 03-1-next-prerendering/pages/index.js
+++ 03-1-next-prerendering/pages/index.js
@@ -1,5 +1,11 @@
+import Link from "next/link";
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Link href={"/users"}>Users Page</Link>
+    </>
+  );
 };
 
 export default HomePage;
