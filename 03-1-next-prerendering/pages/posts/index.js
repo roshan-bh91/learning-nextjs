162f86b89c94f31d37bb5249c78e867428cf4e4c
roshan-bh91 <roshan@betterhalf.ai>
2023-12-22 13:34:24 +0530

    [refactor]: shifted the number from 10 to 4 for list of posts

--- 03-1-next-prerendering/pages/posts/index.js
+++ 03-1-next-prerendering/pages/posts/index.js
@@ -21,7 +21,7 @@ export async function getStaticProps() {
   const jsonFormattedData = await apiResponse.json();
   return {
     props: {
-      postsList: jsonFormattedData.slice(0, 10),
+      postsList: jsonFormattedData.slice(0, 4),
     },
   };
 }
